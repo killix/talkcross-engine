5c63548a10cb27b9ec4b878c199beae869f8c42d
Jiang Lu <jianglu@xiaomi.com>
2016-07-22 22:26:46 +0800

    Create default vue brunch project

---
+++ app/initialize.js
@@ -0,0 +1,68 @@
+import Vue from 'vue';
+import 'babel-polyfill';
+import store from './state/store';
+import VueRouter from 'vue-router';
+import routes from './config/routes';
+import VueResource from 'vue-resource';
+import VueValidator from 'vue-validator';
+
+//
+// Vue extensions
+// Here we tell Vue that we want to add extra functionality to the application.
+//
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+Vue.use(VueValidator);
+
+// 
+// Enviromnment
+// 
+
+const environment = process.env.NODE_ENV;
+
+// 
+// Configure Vue
+// Here we will configure Vue based on the environment we are running in.
+// 
+
+Vue.config.debug = (environment === 'development');
+Vue.config.devtools = (environment === 'development');
+
+// 
+// Application
+// Here we will create the application instance.
+// 
+
+let application = {
+    store
+};
+
+// 
+// Router
+// Here we will create the router instance.
+// 
+
+let router = new VueRouter({
+    history: true
+});
+
+// 
+// Router mapping.
+// Here we will tell VueRouter what routes the application will be using.
+// 
+
+router.map(routes);
+
+// 
+// Start the application
+// Finally, we can start the application.
+// 
+
+router.start(application, '#content', function() {
+    
+    if (window) {
+        window.App = router.app;
+    }
+
+});
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Brunch with Vue
+
+A skeleton application utilizing vue, vuex, vue-resource and vue-router.
+
+## Instructions
+
+To get started, simply run the following command in your terminal...
+
+```bash
+brunch new --skeleton nblackburn/brunch-with-vue
+```
+
+## Donations
+
+If you found this plugin to be useful, please consider [donating](https://paypal.me/nblackburn).
+
+## License
+
+This component is licensed under [MIT](), see [license.md](license.md) for details.
