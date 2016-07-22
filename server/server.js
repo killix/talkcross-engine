4532e39de14bd8a7bdd4f2906f62a487ddecf773
Jiang Lu <jiangl@xiaomi.co>
2016-07-23 00:52:27 +0800

    Setup custom brunch server

--- server/server.js
+++ server/server.js
@@ -19,10 +19,12 @@ app.listen(PORT, function () {
   console.log('Node app is running, port:', PORT);
 
   // 注册全局未捕获异常处理器
-  process.on('uncaughtException', function(err) {
+  process.on('uncaughtException', function (err) {
     console.error("Caught exception:", err.stack);
   });
-  process.on('unhandledRejection', function(reason, p) {
+  process.on('unhandledRejection', function (reason, p) {
     console.error("Unhandled Rejection at: Promise ", p, " reason: ", reason.stack);
   });
 });
+
+module.exports = app;
